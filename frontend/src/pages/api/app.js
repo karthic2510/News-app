4829ebdb4e9d7c700f78c705555906f0246a7296
Karthic Subramanian <karthic.subramanian.25@gmail.com>
2023-03-25 07:55:14 -0400

    Update app.js

--- frontend/src/pages/api/app.js
+++ frontend/src/pages/api/app.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function handler(req, res) {
   const { topic } = req.body;
   try {
-    const response = await axios.post('http://127.0.0.1:5000/app/summarize', { topic });
+    const response = await axios.post('api/summarize', { topic });
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
